f43f19b2bd7cdc58b4a11080fce9ea2ff04c047d
Kholekile Somagaca <kholekile.s@gmail.com>
2024-08-21 17:52:17 +0200

    ✨fix(react-app): installed axios for http request

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
+import { TimelineItem } from './models/timeline.interface';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState<Array<TimelineItem>>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://arthurfrost.qflo.co.za/php/getTimeline.php')  // Replace with your endpoint URL
+      .then(response => {
+        setData(response.data.Timeline);  // Update state with the fetched data
+        setLoading(false);  // Set loading to false once data is fetched
+      })
+      .catch(err => {
+        console.error('Error fetching data:', err);  // Log the error
+        setError(err);  // Set error in state
+        setLoading(false);  // Set loading to false if there's an error
+      });
+  }, []);  // Empty dependency array means this runs once after the initial render
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;  // Display error message if request fails
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data</h1>
+      {data && Array.isArray(data) && data.length > 0 ? (
+        <ul>
+          {data.map((item, index) => (
+            <li key={index}>
+              <label>ID : <strong>{ item.Id}</strong></label>
+              <label>Title : <strong>{ item.Title}</strong></label>
+              <label>Media : <strong>{ item.Media}</strong></label>
+              <label>Description : <strong>{ item.Description}</strong></label>
+              <label>Image : <strong>{ item.Image}</strong></label>
+              <label>Icon : <strong>{ item.Icon}</strong></label>
+              <label>Audio : <strong>{ item.Audio}</strong></label>
+              <label>Remote ID : <strong>{ item.RemoteId}</strong></label>
+              <label>Status : <strong>{ item.Status}</strong></label>
+              <label>IsActive : <strong>{ item.IsActive}</strong></label>
+              <label>InId : <strong>{ item.InId}</strong></label>
+              <label>Create Date : <strong>{ item.CreateDate}</strong></label>
+              <label>Media Name : <strong>{ item.MediaName}</strong></label>
+              <label>Category : <strong>{ item.Category}</strong></label>
+              <label>Epoch : <strong>{ item.Epoch}</strong></label>
+              <label>Audio Size : <strong>{ item.AudioSize}</strong></label>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>No data available</div>
+      )}
     </div>
   );
 }
---
+++ react-app/src/models/timeline.interface.ts
@@ -0,0 +1,18 @@
+export interface TimelineItem {
+    Id: string;
+    Title: string;
+    Media: number;
+    Description: string;
+    Image: string;
+    Icon: string;
+    Audio: string;
+    RemoteId: string;
+    Status: number;
+    IsActive: number;
+    InId: string;
+    CreateDate: string;
+    MediaName: string;
+    Category: string;
+    Epoch: number;
+    AudioSize: number;
+}
